811b168e64434f9e1e6cc2ec05bbe82406b2f554
Henry Felipe <henry.oliveira11@etec.sp.gov.br>
2025-03-21 21:33:02 -0300

    criando projeto pode votar

---
+++ PodeVotar/PodeVotar/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿namespace PodeVotar
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public Pessoa(string pessoaNome, int pessoaIdade) 
+        {
+            Nome = pessoaNome;
+            Idade = pessoaIdade;
+        }
+
+        public bool PodeVotar()
+        {
+            if (this.Idade < 16)
+                return false;
+            else if (this.Idade >= 16 && this.Idade <= 17)
+                return true;
+            else
+                return true;
+        }
+    }
+}
---
+++ PodeVotar/PodeVotar/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace PodeVotar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarregarMenu();
+        }
+
+        public static void CarregarMenu()
+        {
+            Console.Clear();
+
+            DesenharCabecalho();
+            Console.WriteLine("1 - Iniciar programa");
+            Console.WriteLine("0 - Fechar programa");
+            Console.WriteLine("-----------------------");
+
+            Console.Write("\n\nSelecione uma opção: ");
+            string opcaoMenu = Console.ReadLine();
+
+            switch (opcaoMenu)
+            {
+                case "1":
+                    CriarPessoa();
+                    break;
+                case "0":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    CarregarMenu();
+                    break;
+            }
+        }
+
+        public static void DesenharCabecalho()
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Descubra se pode votar!");
+            Console.WriteLine("-----------------------");
+        }
+
+        public static void MostrarDadosPessoa(string pessoaNome, int pessoaIdade)
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine($"Nome: {pessoaNome} \nIdade: {pessoaIdade}");
+            Console.WriteLine("-----------------------");
+        }
+
+        public static void CriarPessoa()
+        {
+            Console.Clear();
+            Console.Write("\nNome Completo: ");
+            string pessoaNome = Console.ReadLine();
+
+            Console.Write("\nIdade: ");
+            int pessoaIdade = Int32.Parse(Console.ReadLine());
+
+            Pessoa pessoa = new Pessoa(pessoaNome, pessoaIdade);
+            bool resultadoVoto = pessoa.PodeVotar();
+
+            Console.Clear();
+            MostrarDadosPessoa(pessoaNome, pessoaIdade);
+
+            if (resultadoVoto && (pessoaIdade >= 16 && pessoaIdade <= 17))
+                Console.WriteLine("\nVoto Opicional!");
+            else if (resultadoVoto)
+                Console.WriteLine("\nVoto Obrigatório!");
+            else
+                Console.WriteLine("\nAinda não pode votar!");
+
+            Console.ReadKey();
+            CarregarMenu();
+        }
+    }
+}
+
